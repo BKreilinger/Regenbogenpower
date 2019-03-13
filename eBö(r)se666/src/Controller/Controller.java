e463691e8094fb0b1c0f3c42b0351ac70a5d7f55
Benny <Benny@Benny>
2019-03-13 21:56:44 +0100

    Forgot to actually choose the Controller class haha (i suck)

---
+++ eBö(r)se666/src/Controller/Controller.java
@@ -0,0 +1,70 @@
+package Controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import Model.Aktien;
+import View.VIEW;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Controller extends Application{
+	private VIEW view;
+	private Aktien a;
+	
+	public Controller(){
+		view = new VIEW();
+		a = new Aktien();
+	}
+	
+	public static void main(String args[]) {
+		launch(null);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		startup(primaryStage);
+		
+	}
+	
+	public void startup(Stage primaryStage) throws ClassNotFoundException, IOException, SQLException {
+		
+		view.start(primaryStage);
+		
+	}
+	
+	public void login(String Benutzername) throws ClassNotFoundException, IOException, SQLException {
+		a.start(Benutzername);
+	}
+	
+	public double returnDAXStand() {
+		return a.returnDAXStand();
+	}
+	public double returnAppleStand() {
+		return a.returnAppleStand();
+	}
+	public double returnVWStand() {
+		return a.returnVWStand();
+	}
+	
+	
+	public String returnDAXR() {
+		return a.returnDAXR();
+	}
+	public String returnAppleR() {
+		return a.returnAppleR();
+	}
+	public String returnVWR() {
+		return a.returnVWR();
+	}
+	
+	public int returnDAXZ() {
+		return a.returnDAXZ();
+	}
+	public int returnAppleZ() {
+		return a.returnAppleZ();
+	}
+	public int returnVWZ() {
+		return a.returnVWZ();
+	}
+}
